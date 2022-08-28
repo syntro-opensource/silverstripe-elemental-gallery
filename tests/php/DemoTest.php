ae0160315ac484dd319d1b904c3ca97024b3fe21
Matthias Leutenegger <mleutenegger@users.noreply.github.com>
2022-08-28 22:47:23 +0200

    nolog: testfix

--- tests/php/DemoTest.php
+++ tests/php/DemoTest.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace Syntro\SEO\Tests;
+namespace Syntro\SilverstripeElementalSlider\Tests;
 
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Dev\FunctionalTest;
 use SilverStripe\CMS\Model\SiteTree;
-use Syntro\SEO\DOM;
 
 /**
  * Demonstartion test
  *
  * @author Matthias Leutenegger <hello@syntro.ch>
  */
-class MetaTest extends FunctionalTest
+class DemoTest extends FunctionalTest
 {
     /**
      * Defines the fixture file to use for this test class
